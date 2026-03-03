377648ceae2614bbbd6542fdc99ae7143eb56845
Leonguyenbk <quangtuan2307@gmail.com>
2026-03-03 16:59:04 +0700

    fix admin require

--- app/decorators.py
+++ app/decorators.py
@@ -1,13 +1,14 @@
 from functools import wraps
 from flask import jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
 from app.models import User, UserRole
 
 def admin_required(fn):
     @wraps(fn)
-    @jwt_required()
     def wrapper(*args, **kwargs):
-        me = User.query.get(int(get_jwt_identity()))
+        verify_jwt_in_request()
+        identity = get_jwt_identity()
+        me = User.query.get(int(identity))
         if not me or me.role != UserRole.ADMIN:
             return jsonify({"msg": "Không đủ quyền (admin)"}), 403
         return fn(*args, **kwargs)
